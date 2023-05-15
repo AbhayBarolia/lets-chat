6b80aaad4be579dc6b9d030d7b00e771356a40f9
AbhayBarolia <abhay28barolia@gmail.com>
2023-05-16 00:54:52 +0530

    first commit

---
+++ frontend/signup/signup.js
@@ -0,0 +1,35 @@
+window.addEventListener("submit", (e)=>{
+    if(e.target.className==="form"){
+        e.preventDefault();
+        let email = document.getElementById("email").value;
+        let mnumber= document.getElementById("mnumber").value;
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+        let password2 = document.getElementById("password2").value;
+        if(password!= password2){
+            alert("Passwords do not match");
+        }
+        else if(username == "" || email == "" || mnumber=="" || password == "" || password2 == ""){
+            alert("All fields are required");
+        }
+        else{
+            let user = {
+                username: username,
+                email: email,
+                mnumber:mnumber,
+                password: password
+            }
+            createUser(user);
+        }
+    }
+  });
+
+
+
+
+
+async function createUser(user){
+    const res= await axios.post("http://18.119.162.42:3000/user/signup",user);   
+    alert(res.data.message);
+    window.location.replace("/login/login.html");
+}

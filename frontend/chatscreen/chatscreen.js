9d294623d72597e02ed34babceab164e66f1bd7b
AbhayBarolia <abhay28barolia@gmail.com>
2023-05-17 05:08:36 +0530

    live chat enabled with continued message flow

--- frontend/chatscreen/chatscreen.js
+++ frontend/chatscreen/chatscreen.js
@@ -1,20 +1,24 @@
 const config={headers:{'Content-Type':'application/JSON',Authorization:localStorage.getItem('token')}};
 let chatlist = document.getElementById("chatlist");
+
+
 window.addEventListener("DOMContentLoaded", (event) => {  
-        let flag = localStorage.getItem("loggedIn");
-        
-        if(flag=="true"){
-            getMessage();
-        }
-        else if(flag=="false"){
-            localStorage.setItem("loggedIn",true);
-            sendMessage("logged in");
-            getMessage();
-        }
-        else{
-            window.location.href = "../login/login.html";
-        }
-    });
+    let flag = localStorage.getItem("loggedIn");
+    
+    if(flag=="true"){
+       // getMessage();
+    }
+    else if(flag=="false"){
+        localStorage.setItem("loggedIn",true);
+        sendMessage("logged in");
+        //getMessage();
+    }
+    else{
+        window.location.href = "../login/login.html";
+    }
+});
+
+
 
     window.addEventListener("submit", (e)=>{
         e.preventDefault();
@@ -49,9 +53,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
       }
 
-      async function getMessage(){
+
+
+    let getMessage = setInterval(async()=>{
         try{
+            
+            chatlist.textContent = '';
             let chatMessages = await axios.get("http://localhost:3000/chat/message", config);
+            
             if(chatMessages)
             {
                 for(let i=0;i<chatMessages.data.messages.length;i++)
@@ -60,23 +69,30 @@ window.addEventListener("DOMContentLoaded", (event) => {
                     showData(str);
                 }
             }
+            
+            var chatHistory = document.getElementById("scroll");
+            chatHistory.scrollTo(-1, chatHistory.scrollHeight);
+            
+            return;
         }
         catch(err){
             alert(err.message);
         }        
-      }
+    },1000);
 
 
-      function showData(str)
+    function showData(str)
     {
         
         
         let msg = document.createElement("li");
-            msg.setAttribute("class","msg");
-            msg.appendChild(document.createTextNode(str));
-            chatlist.appendChild(msg);
-            let hr = document.createElement("hr");  
-            chatlist.appendChild(hr);
+        msg.setAttribute("class","msg");
+        msg.appendChild(document.createTextNode(str));
+        chatlist.appendChild(msg);
+        let hr = document.createElement("hr");  
+        chatlist.appendChild(hr);
         
          
     }
+
+
